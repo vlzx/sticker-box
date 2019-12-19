87a865fc2406382a76f5191788c7b55cd17e5988
zxryo <37045706+zxryo@users.noreply.github.com>
2019-12-19 20:02:48 +0800

    Prototype
    Basic login function for dev

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+    appid: 'wxba7b028af53fe8d9',
+    secret: '4fca4b117bb338459862a9c06b411cc5',
+
+    port: 3443,
+
+    certPath: '/mnt/c/Users/Leung/Documents/tls/localhost.crt',
+    keyPath: '/mnt/c/Users/Leung/Documents/tls/localhost.key',
+
+    dbConn: {
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '123456',
+        database: 'app'
+    }
+}
+
+module.exports = config
