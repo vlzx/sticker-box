b009c1be315d661e0cc0f18bd8022219ae794c96
zxryo <37045706+zxryo@users.noreply.github.com>
2019-12-19 21:13:25 +0800

    Update config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-    appid: 'wxba7b028af53fe8d9',
-    secret: '4fca4b117bb338459862a9c06b411cc5',
+    appid: 'wx747d160c21468d64',
+    secret: '',
 
-    port: 3443,
+    port: 443,
 
-    certPath: '/mnt/c/Users/Leung/Documents/tls/localhost.crt',
-    keyPath: '/mnt/c/Users/Leung/Documents/tls/localhost.key',
+    certPath: '/etc/letsencrypt/live/fakartist.com/fullchain.pem',
+    keyPath: '/etc/letsencrypt/live/fakartist.com/privkey.pem',
 
     dbConn: {
         host: 'localhost',
